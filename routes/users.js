64c946f350a946cfaa2bcf48884c2eafb2bb5b9b
goolsbyash <goolsbyashley015@gmail.com>
2024-01-11 11:20:00 -0500

    Added POST method with username and email validation for route /api/users.

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,36 @@ const express = require("express");
 const router = express.Router();
 const users = require("../data/users");
 
-router.route("/").get((req, res) => {
-  res.json(users);
-});
+router
+  .route("/")
+  .get((req, res) => {
+    res.json(users);
+  })
+  .post((req, res) => {
+    if (req.body.name && req.body.username && req.body.email) {
+      if (users.find((u) => u.username === req.body.username)) {
+        res.json({ error: "Username Already Taken. Try again!" });
+        return;
+      }
+      // Email address validation needed
+      if (users.find((u) => u.email === req.body.email)) {
+        res.json({
+          error:
+            "This email address is already associated with an account. Try again!",
+        });
+        return;
+      }
 
+      const newUser = {
+        userId: users[users.length - 1].userId + 1,
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+      };
+
+      users.push(newUser);
+      res.json(users[users.length - 1]);
+    } else res.json({ error: "Insufficient Data" });
+  });
 
 module.exports = router;
