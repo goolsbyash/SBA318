a49eca1c7e91a0c224ed54b7500b5a804c298d63
goolsbyash <goolsbyashley015@gmail.com>
2024-01-11 11:03:39 -0500

    Added some user data.

---
+++ data/users.js
@@ -0,0 +1,26 @@
+const users = [
+  {
+    userId: 1,
+    name: "Alice Allen",
+    role: "student",
+    username: "aliceall1",
+    email: "aliceall1@scholar.com",
+  },
+  {
+    userId: 2,
+    name: "Bruce Banner",
+    role: "teacher",
+    username: "bruceban2",
+    email: "bruceban2@instuctor.com",
+  },
+  {
+    userId: 3,
+    name: "Clark Cruz",
+    role: "student",
+    username: "clarkcru3",
+    email: "clarkcru3@scholar.com",
+  },
+];
+
+
+module.exports = users;
