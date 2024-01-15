ea4477e124016ace904824a9b2f502d32a4504d3
goolsbyash <goolsbyashley015@gmail.com>
2024-01-15 14:45:16 -0500

    Added GET route for /:role/:id

--- routes/grades.js
+++ routes/grades.js
@@ -3,10 +3,36 @@ const express = require("express");
 // Create router
 const router = express.Router();
 const grades = require("../data/grades");
+const users = require("../data/users");
 
-router.route("/").get((req, res) => {
-  res.json(grades);
+router.get("/", (req, res) => {
+  res.json({
+    links: [
+      {
+        href: "/teacher/:id",
+        rel: "api",
+        type: "GET",
+      },
+      {
+        href: "/student/:id",
+        rel: "api",
+        type: "GET",
+      },
+    ],
+  });
 });
 
+router.get("/:role/:id", (req, res) => {
+  if (req.params.role == "teacher") {
+    if (users.find((u) => u.userId == req.params.id && u.role == "teacher"))
+      res.json(grades);
+    else res.json({ error: "Invalid User" });
+  } else if (req.params.role == "student") {
+    if (users.find((u) => u.userId == req.params.id && u.role == "student")) {
+      const studentGrades = grades.filter((g) => g.userId == req.params.id);
+      res.json(studentGrades);
+    } else res.json({ error: "Invalid User" });
+  } else res.json({ error: "Invalid User" });
+});
 
 module.exports = router;
