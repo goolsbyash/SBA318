62217a1c9cb6ccf31c560e90dc13f4ba7adaba65
goolsbyash <goolsbyashley015@gmail.com>
2024-01-19 11:02:58 -0500

    Corrected route to return view template based on :role.

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,12 +1,28 @@
 // Import Express
 const express = require("express");
+const pug = require("pug");
+
 // Create router
 const router = express.Router();
-const profiles = require("../data/profiles");
+const studentProfile = pug.compileFile("./views/student.pug");
+const teacherProfile = pug.compileFile("./views/teacher.pug");
+const profile = require("../data/profiles");
 
-router.route("/").get((req, res) => {
-  res.json(profiles);
+router.route("/:role/:id").get((req, res) => {
+  let currentProfile = profile.find((p) => p.userId == req.params.id);
+  const options = {
+    userName: currentProfile.name,
+    profilePic: currentProfile.profilePic,
+    content: currentProfile.alerts,
+    gradeView: `/api/grades/${req.params.role}/${req.params.id}`,
+  };
+  // Load student profile template
+  if (req.params.role == "student") {
+    res.send(studentProfile(options));
+  }
+  // Load teacher profile template
+  else if (req.params.role == "teacher") res.send(teacherProfile(options));
+  else res.status(404).send({error: "Invalid User!"});
 });
-
 
 module.exports = router;
