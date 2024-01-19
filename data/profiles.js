bdcbf219c817e93a25f4079169362e47a9a9c7d8
goolsbyash <goolsbyashley015@gmail.com>
2024-01-18 21:41:32 -0500

    Added userId field for querying purposes.

--- data/profiles.js
+++ data/profiles.js
@@ -1,16 +1,25 @@
 const profiles = [
   {
+    userId: 1,
     name: "Alice Allen",
     profilePic: "",
     alerts:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec",
   },{
+    userId: 2,
     name: "Bruce Banner",
     profilePic: "",
     alerts:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec",
   },{
+    userId: 3,
     name: "Clark Cruz",
+    profilePic: "",
+    alerts:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec",
+  },{
+    userId: 4,
+    name: "Darla Daniels",
     profilePic: "",
     alerts:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nec",
